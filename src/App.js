c099b35329d47949b85f3c3fa07377d2c2e3297a
Samueelx <gsammygitau@gmail.com>
2023-01-23 21:55:57 +0300

    fetch data from the server into the App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import BotCollection from './components/BotCollection';
+
+const BOTS_URL = "http://localhost:8001/bots"
 
 function App() {
+  const [bots, setBots] = useState([]);
+
+  function fetchBots(){
+    fetch(BOTS_URL).then(res => res.json())
+    .then(data => console.log(data));
+  }
+
+  useEffect(() => {
+    fetchBots();
+  }, []);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-center text-green-500 font-bold text-3xl">
         Hello world!
       </h1>
+      <BotCollection />
     </div>
   );
 }
